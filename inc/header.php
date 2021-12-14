4b157c932d3e1c8e46820dfdd631ee4424b1fcdd
TG <rora115@naver.com>
2021-12-14 16:05:34 +0900

    add: item log

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
       <a class="navbar-brand" href="http://soojeong.kro.kr"><img src="/asset/img/choice.jpg" width="40px"> FiveM Choice</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,21 +6,30 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
-		  <? if (isset($_SESSION['choice_id'])) { ?>
-      <? if (isset($_SESSION['isadmin'])) {?>
-        <li class="nav-item px-2">
-          <a class="nav-link" href="/adm/items/">아이템 관리</a>
-        </li>
-      <? } ?>
-		  <li class="nav-item dropdown px-2">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownCommunity" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			        <span class="mb-0 text-sm  font-weight-bold"><?= $_SESSION['choice_nickname'] ?></span>
+        <? if (isset($_SESSION['choice_id'])) { ?>
+
+          <? if (isset($_SESSION['isadmin'])) {?>
+            <li class="nav-item dropdown px-2">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownItems" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mb-0 text-sm">아이템 관리</span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownItems">
+                <a class="dropdown-item" href="/adm/items/giveItem.php">아이템 지급</a>
+                <a class="dropdown-item" href="/adm/items/giveLog.php">아이템 지급 로그</a>
+                <a class="dropdown-item" href="/adm/items/quickLog.php">퀵배송 로그</a>
+              </div>  
+            </li>
+          <? } ?>
+
+          <li class="nav-item dropdown px-2">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMypage" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="mb-0 text-sm  font-weight-bold"><?= $_SESSION['choice_nickname'] ?></span>
             </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownCommunity">
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMypage">
               <a class="dropdown-item" href="/logout.php">로그아웃</a>
             </div>
           </li>
-		  <? } else { ?>
+        <? } else { ?>
 		  <li class="nav-item px-2">
             <a class="nav-link" href="/login.php">로그인</a>
           </li>
---
+++ lib/_itemlog.php
@@ -0,0 +1,57 @@
+<?
+$GLOBALS['ret_type'] = basename(__FILE__) == basename($_SERVER["SCRIPT_NAME"]) ? 'ajax' : '';
+include_once $_SERVER['DOCUMENT_ROOT'].'/lib/db_function.php';
+
+function fAPI() {
+	$page = (int)(@$_GET['page'] ?: 1);
+	$user_id = (int)(@$_GET['user_id'] ?: 0);
+	$where = '';
+	if ($user_id) {
+		$where = "AND (g.give_id = $user_id OR g.take_id = $user_id)";
+	}
+	$max_row = 20;
+	$offset = ($page - 1) * $max_row;
+
+    $filename = strtolower(basename($_SERVER["SCRIPT_NAME"]));
+    if ($filename == 'givelog.php') {
+        $flag = '원격배송';
+    } elseif ($filename == 'quicklog.php') {
+        $flag = '퀵배송';
+    } else return;
+
+	$r_cnt = libQuery("
+		SELECT COUNT(*) AS cnt
+		FROM choice_giveitem AS g
+		WHERE g.flag = '$flag' $where
+	");
+
+	if ($r_cnt[0]['cnt'] < 1) {
+		echo "<script>alert('고유번호를 다시 확인해주세요.');</script>";
+	}
+
+	$r_list = libQuery("
+		SELECT g.*, a.nickname AS give_nickname, b.nickname AS take_nickname
+		FROM choice_giveitem AS g
+        LEFT JOIN choice_account AS a ON a.user_id = g.give_id
+        LEFT JOIN choice_account AS b ON b.user_id = g.take_id
+		WHERE g.flag = '$flag' $where
+		ORDER BY send_date DESC
+		LIMIT ?
+		OFFSET ?
+	", 'ii', array($max_row, $offset));
+
+	$r_is_exist_next = $max_row * $page < $r_cnt[0]['cnt'];
+
+	$page > 1 
+		? $prev = $page-1 : $prev="";
+	$r_is_exist_next
+		? $next = $page+1 : $next="";
+
+
+	
+
+	return libReturn('log_list', array('max_row'=>$max_row, 'page'=>$page, 'tot_cnt'=>ceil($r_cnt[0]['cnt'] / $max_row), 'prev'=>$prev, 'next'=>$next, 'list'=>$r_list));
+
+}
+return fAPI();
+?>
